74c00407797c1fe476b4d4ebe26f153c0b6f1fcc
MAC <serikjose@gmail.com>
2019-05-31 10:50:48 -0300

    Config saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -3,16 +3,7 @@ import {
 } from 'redux-saga/effects';
 import api from '~/services/api';
 
-function* login() {
-  try {
-    const res = yield call(api.get, '/users/erikjose');
-
-    console.tron.log(res.data);
-  } catch (err) {
-    console.tron.log('Error', err);
-  }
-}
-
 export default function* rootSaga() {
-  return yield all([takeLatest('SET_QUERY', login)]);
+  // return yield all([takeLatest('SET_QUERY', login)]);
+  return yield all([ () => {} ]);
 }
